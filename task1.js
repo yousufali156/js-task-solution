55768bdf1f6b6685d0c72e4e94c1646e8012c806
yousufali156 <yousufwebwork@gmail.com>
2025-02-10 05:32:40 -0500

    task 1-4

---
+++ task1.js
@@ -0,0 +1,9 @@
+var orange = 350 ;
+var apples =  350 ;
+var totalPrice = orange + apples ;
+console.log(totalPrice) ;
+
+var customerExchange = 1000 ;
+var shopExchange = 700 ;
+var allPaid = customerExchange - shopExchange ;
+console.log(allPaid) ;
---
+++ task2.js
@@ -0,0 +1,13 @@
+var Mathematics = parseFloat(75.25) ;
+var Biology = 65 ;
+var Chemistry = 80 ;
+var Physics = parseFloat(35.45);
+var Bangla = parseFloat(35.45) ;
+
+var totalNumber = Mathematics + Biology + Chemistry + Physics + Bangla ;
+console.log(totalNumber) ;
+
+ gorMaan = totalNumber / 5 ;
+
+ console.log(gorMaan) ;
+ 
